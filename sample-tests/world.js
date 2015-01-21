0683908523306fac335ecc4f23e0c6bfff914fdd
sam@hatoum.net <sam@hatoum.net>
2015-01-21 09:09:33 -0800

    simplified the world config

--- sample-tests/world.js
+++ sample-tests/world.js
@@ -8,26 +8,17 @@
 
     this.World = function (next) {
 
-      var world = helper.world = this;
+      helper.world = this;
 
-      world.cucumber = Package['xolvio:cucumber'].cucumber;
-      world.wdio = Package['xolvio:webdriver'].wdio;
+      helper.world.cucumber = Package['xolvio:cucumber'].cucumber;
 
-      var options = {
-        desiredCapabilities: {browserName: 'PhantomJs'},
-        port: 4444,
-        logLevel: 'silent'
-      };
-
-      world.wdio.getGhostDriver(options, function (browser) {
-        world.browser = browser;
+      Package['xolvio:webdriver'].wdio.getGhostDriver(options, function (browser) {
+        helper.world.browser = browser;
         browser.call(next);
       });
-
 
     };
 
   };
-
 
 })();
