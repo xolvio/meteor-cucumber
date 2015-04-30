2218355f06db3d5d824418f4050809653b8ad5e0
sam@hatoum.net <sam@hatoum.net>
2015-04-29 19:56:17 -0700

    not using promises until fully ready

--- test-app/tests/cucumber/features/step_definitions/my_steps.js
+++ test-app/tests/cucumber/features/step_definitions/my_steps.js
@@ -7,13 +7,11 @@
     // You can use normal require here, cucumber is NOT run in a Meteor context (by design)
     var url = require('url');
 
-    this.Given(/^I have authored the site title as "([^"]*)"$/, function (title) {
-      // no callbacks! DDP has been promisified so you can just return it
-      return this.ddp.callAsync('updateTitle', [title]); // this.ddp is a connection to the mirror
+    this.Given(/^I have authored the site title as "([^"]*)"$/, function (title, callback) {
+      this.ddp.call('updateTitle', [title], callback); // this.ddp is a connection to the mirror
     });
 
     this.When(/^I navigate to "([^"]*)"$/, function (relativePath, callback) {
-      // WebdriverIO supports Promises/A+ out the box, so you can return that too
       this.browser. // this.browser is a pre-configured WebdriverIO + PhantomJS instance
         url(url.resolve(process.env.HOST, relativePath)). // process.env.HOST always points to the mirror
         call(callback);
