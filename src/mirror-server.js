98e0e4501ec219c90643127dfc89dba335d74993
sam@hatoum.net <sam@hatoum.net>
2015-05-06 22:01:24 -0700

    logging as requested in issue #81

--- src/mirror-server.js
+++ src/mirror-server.js
@@ -322,6 +322,11 @@ DEBUG = !!process.env.VELOCITY_DEBUG;
         insertDots = false,
         DOTS = '  ...\n';
 
+    if (errorMessage.indexOf('Timed out waiting for asyncrhonous') !== -1) {
+      return errorMessage.substring(errorMessage.lastIndexOf('->') + 3, errorMessage.length).trim()
+        + ' timed out';
+    }
+
     try {
       _.each(errorMessage.split('\n'), function (line, index) {
         line = line.trim();
