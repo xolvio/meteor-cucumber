91a054f79902af40ad88e4b71f60719a73aeefce
Sam Hatoum <sam@xolv.io>
2015-07-21 17:42:41 -0700

    using promises

--- test-app/tests/cucumber/features/step_definitions/my_steps.js
+++ test-app/tests/cucumber/features/step_definitions/my_steps.js
@@ -11,17 +11,16 @@
       return this.mirror.call('updateTitle', title); // this.ddp is a connection to the mirror
     });
 
-    this.When(/^I navigate to "([^"]*)"$/, function (relativePath, callback) {
+    this.When(/^I navigate to "([^"]*)"$/, function (relativePath) {
       this.browser. // this.browser is a pre-configured WebdriverIO + PhantomJS instance
-        url(url.resolve(process.env.ROOT_URL, relativePath)). // process.env.ROOT_URL always points to the mirror
-        call(callback);
+        url(url.resolve(process.env.ROOT_URL, relativePath)); // process.env.ROOT_URL always points to the mirror
     });
 
     this.Then(/^I should see the heading "([^"]*)"$/, function (expectedTitle, callback) {
       // you can use chai-as-promised in step definitions also
-      this.browser.
+      return this.browser.
         waitForVisible('h1'). // WebdriverIO chain-able promise magic
-        getText('h1').should.become(expectedTitle).and.notify(callback);
+        getText('h1').should.become(expectedTitle);
     });
 
   };
