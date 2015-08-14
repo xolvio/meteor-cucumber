f6318401e625f414efdafdcb065d616a4f3ea872
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-08-14 22:05:42 +0200

    Simplified sample steps

--- src/sample-tests/steps.js
+++ src/sample-tests/steps.js
@@ -1,33 +1,26 @@
-(function () {
+'use strict';
 
-  'use strict';
+// You can include npm dependencies for support files in tests/cucumber/package.json
+var _ = require('underscore');
+// You can use normal require here, cucumber is NOT run in a Meteor context (by design)
+var url = require('url');
 
-  // You can include npm dependencies for support files in tests/cucumber/package.json
-  var _ = require('underscore');
+module.exports = function () {
 
-  module.exports = function () {
+  this.Given(/^I am a new user$/, function () {
+    server.call('reset'); // this.ddp is a connection to the mirror
+  });
 
-    // You can use normal require here, cucumber is NOT run in a Meteor context (by design)
-    var url = require('url');
+  this.When(/^I navigate to "([^"]*)"$/, function (relativePath) {
+    // client is a pre-configured WebdriverIO + PhantomJS instance
+    // process.env.ROOT_URL always points to the mirror
+    client.url(url.resolve(process.env.ROOT_URL, relativePath));
+  });
 
-    this.Given(/^I am a new user$/, function () {
-      // no callbacks! DDP has been promisified so you can just return it
-      return this.server.call('reset'); // this.ddp is a connection to the mirror
-    });
+  this.Then(/^I should see the title "([^"]*)"$/, function (expectedTitle) {
+    // you can use chai in step definitions also
+    client.waitForExist('title');
+    expect(client.getTitle()).to.equal(expectedTitle);
+  });
 
-    this.When(/^I navigate to "([^"]*)"$/, function (relativePath) {
-      // WebdriverIO supports Promises/A+ out the box, so you can return that too
-      return this.client. // this.browser is a pre-configured WebdriverIO + PhantomJS instance
-        url(url.resolve(process.env.ROOT_URL, relativePath)); // process.env.ROOT_URL always points to the mirror
-    });
-
-    this.Then(/^I should see the title "([^"]*)"$/, function (expectedTitle) {
-      // you can use chai-as-promised in step definitions also
-      return this.client.
-        waitForVisible('body *'). // WebdriverIO chain-able promise magic
-        getTitle().should.become(expectedTitle);
-    });
-
-  };
-
-})(); 
+};
